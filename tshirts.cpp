9435b0d38cb31beca4f2ed5b48f2e37e3be5bcb0
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-24 19:40:21 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -17,7 +17,6 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(-1) == '\0');
     assert(size(38) == 'S');
     assert(size(42) == 'M');
     
