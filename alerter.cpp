1026960cb125e0fce8d32676988feb154ed45050
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-25 16:29:48 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -6,11 +6,11 @@ int alertFailureCount = 0;
 int alertRequest = 0;
 
 int networkAlertStub(float celcius) {
+      std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
     if(celcius>MAX_Temp)
     {
         return 500;
     }
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
