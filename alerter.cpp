63748330cb1f7effe3e1d2a675a9016f480c10fb
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-25 14:58:18 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -3,7 +3,7 @@
 
 float MAX_Temp = 200;
 int alertFailureCount = 0;
-int alretRequest = 0;
+int alertRequest = 0;
 
 int networkAlertStub(float celcius) {
     if(celcius>MAX_Temp)
@@ -27,14 +27,14 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-    alretRequest++;
+    alertRequest++;
 }
 
 int main() {
     alertInCelcius(400.5);
-    assert(alertFailureCount , alretRequest);
+    assert(alertFailureCount == alertRequest);
     alertInCelcius(303.6);
-    assert(alertFailureCount, alretRequest);
+    assert(alertFailureCount == alertRequest);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
