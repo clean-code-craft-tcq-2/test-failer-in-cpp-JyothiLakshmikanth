afd3ea68cb75a7fc8a0c68fa9a15157ad7362750
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-25 16:03:51 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <sstream>
 #include <assert.h>
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int calcPair(int majorIdx, int minorIdx)
+{
+   return majorIdx * 5 + minorIdx;
+}
+void printConsole(int majorIdx, int minorIdx)
+{
+    ::std::string output = majorColor[majorIdx];
+    char ch = '|';
+    output+=ch;
+    std::cout << calcPair(majorIdx, minorIdx) <<ch
+    << output.append(minorColor[majorIdx] ) << "\n";
+}
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            printConsole(i,j;
         }
     }
     return i * j;
@@ -16,6 +29,14 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    std::ostringstream oss;
+    std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+    printConsole(0,2);
+    std::cout.rdbuf(p_cout_streambuf); // restore
+    // test  oss content...
+    std::cout << oss.str()<<std::endl;
+    assert(oss && oss.str() == "2|White|Green");
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
