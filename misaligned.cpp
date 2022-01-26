6e5e3b4ac284952047115f03502f0f3ef7683212
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-26 22:30:47 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -3,24 +3,32 @@
 #include <assert.h>
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+ std::ostringstream oss;
+ std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+ std::cout.rdbuf(oss.rdbuf());
 int calcPair(int majorIdx, int minorIdx)
 {
    return majorIdx * 5 + minorIdx;
 }
-void printConsole(int majorIdx, int minorIdx)
+::std::string getCombination(int majorIdx, int minorIdx)
 {
-//     ::std::string output = majorColor[majorIdx];
-     char ch = '|';
-//     output+=ch;
-//     std::cout << calcPair(majorIdx, minorIdx) <<ch<< output.append(minorColor[majorIdx] ) << "\n";
-   std::cout << calcPair(majorIdx, minorIdx) << ch << majorColor[majorIdx] << ch << minorColor[majorIdx]  << "\n";
+    ::std::string output = ::std::to_string(calcPair(majorIdx, minorIdx));
+        char ch = '|';
+    output+=ch;
+    output.append(majorColor[majorIdx]);
+    output+=ch;
+    output.append(minorColor[majorIdx] );
+return output;
+   
+//   std::cout << calcPair(majorIdx, minorIdx) << ch << majorColor[majorIdx] << ch << minorColor[majorIdx]  << "\n";
 }
 
 int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printConsole(i,j);
+            ::std::string output = getCombination(i,j);
+             std::cout << output << "\n";
         }
     }
     return i * j;
@@ -29,14 +37,9 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
-    std::ostringstream oss;
-    std::streambuf* p_cout_streambuf = std::cout.rdbuf();
-    std::cout.rdbuf(oss.rdbuf());
-    printConsole(0,2);
-    std::cout.rdbuf(p_cout_streambuf); // restore
-    // test  oss content...
-    std::cout << oss.str()<<std::endl;
-    assert(oss && oss.str() == "2|White|Green");
+    // test 
+     std::cout << getCombination(0,2)<<std::endl;
+     assert(getCombination(0,2) == "2|White|Green");
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
