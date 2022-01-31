7c4ee5ecece77a6a85c88439a5ab74c5a337811b
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-31 20:37:07 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,12 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <sstream>
 #include <assert.h>
