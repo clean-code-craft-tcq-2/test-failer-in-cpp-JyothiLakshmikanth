329e2548e954a6c3a9247b0cc57371a6014fafa9
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-02-03 19:34:29 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -6,18 +6,11 @@ float MAX_Temp = 200;
 int alertFailureCount = 0;
 float acceptedDiff = 0.001;
 
-int checkForThreshold(float celcius)
-{
-    if(celcius>MAX_Temp)
-    {
-        return 500;
-    }
-    return 200;
-}
 float convertToCelcius(float farenheit)
 {
     return (farenheit - 32) * 5 / 9;
 }
+
 void countFailureCases(int returnCode)
 {
    if (returnCode != 200) 
@@ -25,27 +18,25 @@ void countFailureCases(int returnCode)
         alertFailureCount += 1;
    }
 }
+
 void alertInCelcius(float farenheit, int(*funAlerter)(float)) {
     float celcius = convertToCelcius(farenheit);
     int returnCode = funAlerter(celcius);
     countFailureCases(returnCode);
 }
+
 int networkAlertStub(float celcius) {
-    int value = checkForThreshold(celcius);
-    ::std::cout<<"value "<<value<<::std::endl;
-    return value;
+    if(celcius>MAX_Temp)
+    {
+        return 500;
+    }
+    return 200;
 }
+
 void testConversion(float f, float expectedValue)
 {
     float receivedValue = convertToCelcius(f);
-    std::cout <<receivedValue<<::std::endl;
-    std::cout <<expectedValue<<::std::endl;
-    std::cout <<(receivedValue-expectedValue)<<::std::endl;
     assert((receivedValue-expectedValue) < acceptedDiff);
-}
-void testTheThreshold(float c, int expectedoutput)
-{
-    assert(checkForThreshold(c) == expectedoutput);
 }
 
 void testAlerter(float testValue, int expectedCount)
@@ -60,8 +51,6 @@ int main() {
     testAlerter(503.6, 2);
     testConversion(400.5, 204.722);
     testConversion(303.6, 150.899);
-    testTheThreshold(204.722, 500);
-    testTheThreshold(150.899, 200);
     std::cout << "All is well (maybe!)\n";
 
     return 0;
